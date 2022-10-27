0908310cd039e20b77edbf8f3e7d74622112e90e
User <User@DESKTOP-49HMLNI>
2022-10-27 21:56:25 +0800

    2

--- TaiwainStock.Utility/TaiwainStockUtility.cs
+++ TaiwainStock.Utility/TaiwainStockUtility.cs
@@ -22,11 +22,11 @@ namespace TaiwainStock.Utility
 			int weekDay = (int)dt.DayOfWeek;
 
 			// 營業時間
-			DateTime starTime = new DateTime(dt.Year, dt.Month, dt.Day, 9, 0, 0);
+			DateTime startTime = new DateTime(dt.Year, dt.Month, dt.Day, 9, 0, 0);
 			DateTime endTime = new DateTime(dt.Year, dt.Month, dt.Day, 13, 30, 0);
 
 			// 如果在營業時間內回true，反則是false
-			if ((weekDay >= 1 && weekDay <= 5) && (dt.CompareTo(starTime) >= 0 && dt.CompareTo(endTime) <= 0))
+			if ((weekDay >= 1 && weekDay <= 5) && (dt.CompareTo(startTime) >= 0 && dt.CompareTo(endTime) <= 0))
 			{ return true; }
 			return false;
 		}
--- TestProject1/TaiwainStockUtilityTests.cs
+++ TestProject1/TaiwainStockUtilityTests.cs
@@ -31,8 +31,12 @@ namespace TestProject1
 			Assert.IsFalse(result);
 		}
 
-		[TestCase("2022/10/27 9:00:00")]
-		[TestCase("2022/10/27 13:30:00")]
+		[TestCase("2022/10/28 9:00:01")]
+		[TestCase("2022/10/27 9:00:01")]
+		[TestCase("2022/10/26 9:00:01")]
+		[TestCase("2022/10/25 9:00:01")]
+		[TestCase("2022/10/24 9:00:01")]
+		[TestCase("2022/10/28 13:30:00")]
 		public void TradingTimeTest_在週一到週五的營業時間內傳回true(string dt)
 		{
 			var obj = new TaiwainStockUtility();
