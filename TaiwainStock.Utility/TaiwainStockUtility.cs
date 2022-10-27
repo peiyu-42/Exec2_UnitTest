0463b6d4e8f65c584ad973986a37f829c9db7241
User <User@DESKTOP-49HMLNI>
2022-10-27 21:25:00 +0800

    加入專案檔案。

---
+++ TaiwainStock.Utility/TaiwainStockUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiwainStock.Utility
+{
+	/// <summary>
+	/// 台灣股市
+	/// </summary>
+	public class TaiwainStockUtility
+	{
+		/// <summary>
+		/// 是否在營業的時間
+		/// </summary>
+		/// <param name="dt">時間</param>
+		/// <returns></returns>
+		public bool IsTradingTime(DateTime dt)
+		{
+			// 判斷是星期幾
+			int weekDay = (int)dt.DayOfWeek;
+
+			// 營業時間
+			DateTime starTime = new DateTime(dt.Year, dt.Month, dt.Day, 9, 0, 0);
+			DateTime endTime = new DateTime(dt.Year, dt.Month, dt.Day, 13, 30, 0);
+
+			// 如果在營業時間內回true，反則是false
+			if ((weekDay >= 1 && weekDay <= 5) && (dt.CompareTo(starTime) >= 0 && dt.CompareTo(endTime) <= 0))
+			{ return true; }
+			return false;
+		}
+	}
+}
---
+++ TestProject1/TaiwainStockUtilityTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaiwainStock.Utility;
+
+namespace TestProject1
+{
+	internal class TaiwainStockUtilityTests
+	{
+		[TestCase("2022/10/29")]
+		[TestCase("2022/10/30")]
+		public void TradingTimeTest_星期六日傳回false(string dt)
+		{
+			var obj = new TaiwainStockUtility();
+
+			bool result = obj.IsTradingTime(Convert.ToDateTime(dt));
+
+			Assert.IsFalse( result);
+		}
+
+		[TestCase("2022/10/27 8:59:59")]
+		[TestCase("2022/10/27 13:30:1")]
+		public void TradingTimeTest_不是營業時間傳回false(string dt)
+		{
+			var obj = new TaiwainStockUtility();
+
+			bool result = obj.IsTradingTime(Convert.ToDateTime(dt));
+
+			Assert.IsFalse(result);
+		}
+
+		[TestCase("2022/10/27 9:00:00")]
+		[TestCase("2022/10/27 13:30:00")]
+		public void TradingTimeTest_在營業的日期與時間內傳回true(string dt)
+		{
+			var obj = new TaiwainStockUtility();
+
+			bool result = obj.IsTradingTime(Convert.ToDateTime(dt));
+
+			Assert.IsTrue(result);
+		}
+	}
+}
